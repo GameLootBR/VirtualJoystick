9b456e24b4421c94d3b6320fd2672237c8b0962c
Luiz Junior <luiz.menezes@gmail.com>
2020-09-02 22:36:26 -0300

    Initial commit

---
+++ Assets/VirtualJoystick/VirtualJoystick.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    public Transform knob;
+    public Vector2 axis;
+
+    private Vector2 joystickSize;
+
+    //--------------------------------------------------
+    void Start()
+    {
+        joystickSize = GetComponent<RectTransform>().rect.max;
+    }
+
+    //--------------------------------------------------
+    private void MoveKnob(Vector2 pos)
+    {
+        knob.position = pos;
+        pos.x = Mathf.Clamp(knob.localPosition.x, 0, joystickSize.x);
+        pos.y = Mathf.Clamp(knob.localPosition.y, 0, joystickSize.y);
+        knob.localPosition = pos;
+
+        axis = ((knob.localPosition / joystickSize) * 2) - Vector2.one;
+    }
+
+    //--------------------------------------------------
+    public void OnDrag(PointerEventData eventData)
+    {
+        MoveKnob(eventData.position);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        MoveKnob(eventData.position);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        axis = Vector2.zero;
+        knob.localPosition = joystickSize/2;
+    }
+}
